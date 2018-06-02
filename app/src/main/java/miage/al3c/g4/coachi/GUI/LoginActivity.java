b1fd2d3a703df3363ffdee875e6705933c864a50
Entrax <entrax.643@gmail.com>
2018-06-02 12:39:13 +0200

    Shared Preferences login

--- app/src/main/java/miage/al3c/g4/coachi/GUI/LoginActivity.java
+++ app/src/main/java/miage/al3c/g4/coachi/GUI/LoginActivity.java
@@ -4,7 +4,9 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -66,6 +68,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private View mProgressView;
     private View mLoginFormView;
 
+    static final String KEY_USERNAME = "username";
+    static final String KEY_PASSWORD = "password";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -96,6 +101,12 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String username = prefs.getString(KEY_USERNAME, "email@domaine.fr");
+        String password = prefs.getString(KEY_PASSWORD, "test");
+        mEmailView.setText(username);
+        mPasswordView.setText(password);
     }
 
     private void populateAutoComplete() {
@@ -335,6 +346,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             // TODO: register the new account here.
 
             Log.d("TESTGUI", "REGISTER HERE");
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(LoginActivity.this);
+            SharedPreferences.Editor ed = prefs.edit();
+            ed.putString(KEY_USERNAME, mEmail);
+            ed.putString(KEY_PASSWORD, mPassword);
+            ed.commit();
             return true;
         }
 
