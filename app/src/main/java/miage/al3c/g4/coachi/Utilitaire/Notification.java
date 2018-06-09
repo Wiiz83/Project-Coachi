699500af786bc0e851cfaee53691d409811c82d2
imaneet <david.toutain@limayrac.fr>
2018-06-09 08:49:08 +0200

    Create class notifs
    Création de la classe pour les notification.

--- app/src/main/java/miage/al3c/g4/coachi/Utilitaire/Notification.java
+++ app/src/main/java/miage/al3c/g4/coachi/Utilitaire/Notification.java
@@ -3,11 +3,14 @@ package miage.al3c.g4.coachi.Utilitaire;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
 
-public class Notification {
+public class Notification extends AppCompatActivity{
     private String id;
     private CharSequence nm;
-    private NotificationChannel nc = new NotificationChannel(id,  nm, NotificationManager.IMPORTANCE_DEFAULT);
-    private NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(Notification.this, nc.getId() );
+    private NotificationChannel nc = new NotificationChannel("channelIdCoachie",  "popupAnim", NotificationManager.IMPORTANCE_DEFAULT);
+    private NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, nc.getId() );
+
+    
 
 }
