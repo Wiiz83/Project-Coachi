5a0c61c057e095895c1d218a996faddb7ba24470
imaneet <david.toutain@limayrac.fr>
2018-06-09 09:36:03 +0200

    ic + notifs
    Ajout du fichier des icones dans les drawables + modifications Notifications

--- app/src/main/java/miage/al3c/g4/coachi/Utilitaire/Notification.java
+++ app/src/main/java/miage/al3c/g4/coachi/Utilitaire/Notification.java
@@ -2,15 +2,55 @@ package miage.al3c.g4.coachi.Utilitaire;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+
+import miage.al3c.g4.coachi.R;
 
 public class Notification extends AppCompatActivity{
     private String id;
-    private CharSequence nm;
+    private String content;
+    private NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
     private NotificationChannel nc = new NotificationChannel("channelIdCoachie",  "popupAnim", NotificationManager.IMPORTANCE_DEFAULT);
     private NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, nc.getId() );
+    // Create an explicit intent for an Activity in your app
+    private Intent intent = new Intent(this, AlertDialog.class);
+    PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
 
-    
 
+
+    public Notification(String id, String ct, String title){
+        setId(id);
+        setContent(ct);
+        mBuilder.setSmallIcon(R.drawable.ic_notifs);
+        mBuilder.setContentText(this.getContent().subSequence(0, this.getContent().length()));
+        mBuilder.setContentTitle(title.subSequence(0, title.length()));
+        mBuilder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        mBuilder.setContentIntent(pendingIntent);
+        mBuilder.setAutoCancel(true);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void sendNotif(int notifId){
+        notificationManager.notify(notifId, mBuilder.build());
+    }
 }
