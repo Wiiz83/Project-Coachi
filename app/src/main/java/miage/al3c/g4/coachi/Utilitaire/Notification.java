d8df2557cbb30193be02371dc46506b5ee132198
imaneet <david.toutain@limayrac.fr>
2018-06-10 14:31:13 +0200

    remove useless import
    remove useless import

--- app/src/main/java/miage/al3c/g4/coachi/Utilitaire/Notification.java
+++ app/src/main/java/miage/al3c/g4/coachi/Utilitaire/Notification.java
@@ -6,7 +6,6 @@ import android.app.PendingIntent;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 
 import miage.al3c.g4.coachi.R;
