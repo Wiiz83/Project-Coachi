653f160541d5cd5ef84380ba71c9e631fd82247b
mh0 <mahdi.hentati@gmail.com>
2018-06-16 19:00:07 +0200

    modif intervalle

--- app/src/main/java/miage/al3c/g4/coachi/TimerChien.java
+++ app/src/main/java/miage/al3c/g4/coachi/TimerChien.java
@@ -18,7 +18,7 @@ public class TimerChien extends CountDownTimer {
     private DEST destiation  = DEST.BED1;
 
     public TimerChien(ControlleurChien3D chien) {
-       super(9999999,10000);
+       super(9999999,15000);
        this.chien = chien;
    }
 
